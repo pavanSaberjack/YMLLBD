63cf765060ef053e71696317b82b70b9b8a8afa2
Mahesh Shanbhag <mahesh.shanbhag@ymedialabs.com>
2013-02-10 06:47:35 +0530

    Added the Twilio!

---
+++ Code/LBD/LBD/Twilio/TwilioDataSource.h
@@ -0,0 +1,33 @@
+//
+//  TwilioDataSource.h
+//  ConcurrentSample
+//
+//  Created by Mahesh on 2/9/13.
+//  Copyright (c) 2013 Mahesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class TCDevice;
+
+@interface TwilioDataSource : NSObject
+
+// the device that connect to the Twilio services
+@property (nonatomic, strong, readonly)TCDevice *device;
+
+-(id)initWithUserName:(NSString *)userName;
+
+/*
+ init the device
+ */
+- (void)initDevice;
+
+/*
+ getting the audio file and the video file
+ */
+
+- (void)listenAudio:(NSURL *)url;
+- (void)recordQuerys;
+- (void)stopAudio;
+
+@end
